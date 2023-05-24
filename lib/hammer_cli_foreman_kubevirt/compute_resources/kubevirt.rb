700d618460fe63129eec209f4c85c387173a94d0
archanaserver <ak31960466@gmail.com>
2023-05-24 10:26:08 +0530

    Fix Rubocop Layout/ArrayAlignment

--- lib/hammer_cli_foreman_kubevirt/compute_resources/kubevirt.rb
+++ lib/hammer_cli_foreman_kubevirt/compute_resources/kubevirt.rb
@@ -38,8 +38,8 @@ module HammerCLIForemanKubevirt
       def provider_specific_fields
         [
           Fields::Field.new(:label => _('hostname'), :path => [:hostname]),
-            Fields::Field.new(:label => _('api_port'), :path => [:api_port]),
-            Fields::Field.new(:label => _('namespace'), :path => [:namespace]),
+          Fields::Field.new(:label => _('api_port'), :path => [:api_port]),
+          Fields::Field.new(:label => _('namespace'), :path => [:namespace]),
         ]
       end
 
