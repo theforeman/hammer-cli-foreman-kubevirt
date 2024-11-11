theforeman/hammer-cli-foreman-kubevirt

commit 1e6288de19cb0c531c068adaacf4f051de56343e

Add shared GHAs for ruby

hammer_cli_foreman_kubevirt.gemspec
@@ -13,10 +13,6 @@ Gem::Specification.new do |s|
   s.files = Dir['{lib,config}/**/*', 'LICENSE', 'README*'] + Dir["locale/**/*.{po,pot,mo}"]
   s.require_paths = ["lib"]
 
-  s.add_dependency 'hammer_cli_foreman', '>=0.17.0'
-  s.add_development_dependency 'gettext', '>= 3.1.3', '< 4.0.0'
-  s.add_development_dependency 'rake', '~> 12.3'
-  s.add_development_dependency 'rdoc'
-
+  s.add_dependency 'hammer_cli_foreman', '~> 3.10'
   s.required_ruby_version = '>= 2.7', '< 4'
 end

.github/workflows/unit_tests.yml
@@ -1,0 +1,17 @@
+name: CI
+
+on:
+  pull_request:
+  push:
+    branches:
+      - 'master'
+
+concurrency:
+  group: ${{ github.ref_name }}-${{ github.workflow }}
+  cancel-in-progress: true
+
+jobs:
+  rubocop:
+    name: Rubocop
+    uses: theforeman/actions/.github/workflows/rubocop.yml@v0
+

.rubocop.yml
@@ -1,87 +1,29 @@
-# TODO: remove this file by either moving cops here or fixing code
 require:
   - rubocop-performance
-
-inherit_from:
-  - .rubocop_todo.yml
+  - rubocop-minitest
 
 AllCops:
-  TargetRubyVersion: 2.7
-  TargetRailsVersion: 5.2
-  Exclude:
-    - 'Gemfile.dev.rb'
+  NewCops: enable
 
-# Don't prefer is_a? over kind_of?
-Style/ClassCheck:
-  Enabled: false
-
-# Don't enforce certain methods, e.g. detect over find
-Style/CollectionMethods:
-  Enabled: false
-
-# Don't enforce documentation
-Style/Documentation:
-  Enabled: false
-
-# Support both ruby19 and hash_rockets
-Style/HashSyntax:
-  Enabled: false
-
-# Both double and single quotes are OK
-Style/StringLiterals:
-  Enabled: false
-
-# Don't enforce frozen string literals
-Style/FrozenStringLiteralComment:
-  Enabled: false
-
-Style/FormatStringToken:
-  Enabled: false
-
-Style/FormatString:
-  Enabled: false
-
-Style/RaiseArgs:
-  Enabled: false
-
-Naming/MethodParameterName:
+Layout/LineLength:
   Enabled: false
 
 Metrics:
   Enabled: false
 
-Performance/Casecmp:
+Style/Documentation:
   Enabled: false
 
-Performance/RedundantMatch:
-  Enabled: true
-
-Performance/RedundantMerge:
-  Enabled: true
-
-#Allow both ['a', 'b'], %w[a b] and %w(a b) style arrays
-Style/WordArray:
+Style/StringLiterals:
   Enabled: false
 
-Style/AndOr:
-  EnforcedStyle: conditionals
-
-Style/TernaryParentheses:
-  EnforcedStyle: require_parentheses_when_complex
-
-Style/InverseMethods:
+Style/FrozenStringLiteralComment:
   Enabled: false
 
-Layout/FirstParameterIndentation:
-  EnforcedStyle: consistent
-
-Bundler/OrderedGems:
+Gemspec/RequireMFA:
   Enabled: false
 
-Style/Alias:
-  EnforcedStyle: prefer_alias_method
-
-Layout/DotPosition:
+Style/SymbolProc:
   Enabled: false
 
 Style/IfUnlessModifier:
@@ -113,4 +55,7 @@ Style/PreferredHashMethods:
   Enabled: false
 
 Style/MultipleComparison:
+  Enabled: false
+
+Style/SymbolArray:
   Enabled: false

.rubocop_todo.yml
@@ -1,30 +1,0 @@
-# This configuration was generated by
-# `rubocop --auto-gen-config`
-# on 2018-11-05 09:40:45 +0200 using RuboCop version 0.60.0.
-# The point is for the user to remove these configuration records
-# one by one as the offenses are removed from the code base.
-# Note that changes in the inspected code, or installation of new
-# versions of RuboCop, may require this file to be generated again.
-
-# Offense count: 1
-Metrics/AbcSize:
-  Max: 21
-
-# Offense count: 1
-Metrics/CyclomaticComplexity:
-  Max: 7
-
-# Offense count: 1
-# Configuration parameters: CountComments, ExcludedMethods.
-Metrics/MethodLength:
-  Max: 24
-
-# Offense count: 1
-Metrics/PerceivedComplexity:
-  Max: 8
-
-# Offense count: 8
-# Configuration parameters: AllowHeredoc, AllowURI, URISchemes, IgnoreCopDirectives, IgnoredPatterns.
-# URISchemes: http, https
-Layout/LineLength:
-  Max: 238

Gemfile
@@ -2,12 +2,10 @@ source "https://rubygems.org"
 dev_gemfile = File.expand_path("Gemfile.dev.rb", __dir__)
 eval_gemfile(dev_gemfile) if File.exist?(dev_gemfile)
 gemspec
+gem 'rake', '~> 10.1.0'
 
 group :rubocop do
-  gem 'rubocop', '~> 1.28.0'
-  gem 'rubocop-performance'
-end
-
-group :test do
-  gem 'rake', '~> 10.1.0'
+  gem 'rubocop-minitest', '~> 0.9.0'
+  gem 'rubocop-performance', '~> 1.5.2'
+  gem 'rubocop-rake', '~> 0.6.0'
 end

Rakefile
@@ -34,13 +34,12 @@ end
 
 begin
   require 'rubocop/rake_task'
+rescue LoadError
+  # RuboCop is optional
+  task default: :test
+else
   RuboCop::RakeTask.new
-rescue StandardError => e
-  puts e.message
-end
-
-task :default do
-  Rake::Task['rubocop'].execute
+  task default: [:rubocop, :test]
 end
 
 require "hammer_cli_foreman_kubevirt/version"

lib/hammer_cli_foreman_kubevirt/compute_resources/kubevirt.rb
@@ -37,9 +37,9 @@ module HammerCLIForemanKubevirt
 
       def provider_specific_fields
         [
-          Fields::Field.new(:label => _('hostname'), :path => [:hostname]),
-          Fields::Field.new(:label => _('api_port'), :path => [:api_port]),
-          Fields::Field.new(:label => _('namespace'), :path => [:namespace])
+          Fields::Field.new(label: _('hostname'), path: [:hostname]),
+          Fields::Field.new(label: _('api_port'), path: [:api_port]),
+          Fields::Field.new(label: _('namespace'), path: [:namespace])
         ]
       end
 

commit 9a995be422c66ac8a1ac455258f621549c3abd6e

Add back development dependencies for translations
These were dropped in 1e6288de19cb0c531c068adaacf4f051de56343e but are
needed to extract translations.

Fixes: 1e6288de19cb0c531c068adaacf4f051de56343e ("Add shared GHAs for ruby")

hammer_cli_foreman_kubevirt.gemspec
@@ -16,5 +16,10 @@ Gem::Specification.new do |s|
   s.require_paths = ['lib']
 
   s.add_dependency 'hammer_cli_foreman', '~> 3.10'
+
+  s.add_development_dependency 'gettext', '>= 3.1.3', '< 4.0.0'
+  s.add_development_dependency 'rake', '~> 12.3'
+  s.add_development_dependency 'rdoc', '~> 6.0'
+
   s.required_ruby_version = '>= 2.7', '< 4'
 end